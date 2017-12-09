34c4c3e12b55d9ab4b18b7528b571b5d413e467d
Matt <magin3191@gmail.com>
2017-12-09 16:00:08 -0700

    added migration progress

--- db/migrations/20171208154254_players.js
+++ db/migrations/20171208154254_players.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('players',(table){
+    table.increments()
+    table.integer('name')
+    table.string('height')
+    table.string('weight')
+    table.string('position')
+
+    )
+
+  })
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('players')
 };
--- db/migrations/20171208154408_teams.js
+++ db/migrations/20171208154408_teams.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('teams',function(table){
+        table.increments()
+        table.string('Abv')
+
+
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('teams')
 };
