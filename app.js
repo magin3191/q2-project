436c76502ee2ca27238747cf024da47c782f75a7
Mirtau <tjj72@comcast.net>
2017-12-10 10:14:36 -0700

    static added, supertest added

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var port = process.env.PORT || 3000
 var environment = process.env.NODE_ENV || 'development'
 var config = require('./knexfile')[environment]
 var knex = require('knex')(config)
-module.exports = knex
 
+app.use(express.static('public'))
 app.listen(port, function() {
   console.log(`listening on port ${port}`)
 })
+
+module.exports = {
+  knex,
+app
+}
