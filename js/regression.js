16caa9f5cea7ec79fac78ac219de330b9cab421d
caelinsheahan <caelinsheahan@gmail.com>
2017-12-10 14:47:15 -0700

    added some libs we need at D3 chart

--- js/regression.js
+++ js/regression.js
@@ -1,5 +1,5 @@
-const regression =require('regression')
-const result = regression.linear([[24,22], [44,2], [33,7]]) //([[stat1(player1), stat2(player1)]]);
+const regression = require('regression')
+const result = regression.linear([[24, 22], [44, 2], [33, 7]]) //([[stat1(player1), stat2(player1)]]);
 const gradient = result.equation[0]
 const yIntercept = result.equation[1]
 
@@ -11,7 +11,9 @@ const playerarr = ['Populated with player names in order of ID']
 console.log(result)
 
 // result looks like this: { points: [ [ 24, 19.81 ], [ 44, 0.21 ], [ 33, 10.99 ] ],
-  // predict: [Function: predict],
-  // equation: [ -0.98, 43.33 ],
-  // r2: 0.89,
-  // string: 'y = -0.98x + 43.33' }
+// predict: [Function: predict],
+// equation: [ -0.98, 43.33 ],
+// r2: 0.89,
+// string: 'y = -0.98x + 43.33' }
+
+module.exports = { result }
