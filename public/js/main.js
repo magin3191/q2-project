401a0e17c5f39b915e83f41fa87ffbe7ed1516f3
Ross <ross.tienken99@gmail.com>
2017-12-11 11:22:25 -0700

    storing/recieving selected stats

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,21 @@
-$('.modal').modal({
-  dismissible: true, // Modal can be dismissed by clicking outside of the modal
-  opacity: .5, // Opacity of modal background
-  inDuration: 300, // Transition in duration
-  outDuration: 200, // Transition out duration
-  startingTop: '4%', // Starting top style attribute
-  endingTop: '10%', // Ending top style attribute
-  ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-  },
-  complete: function() {} // Callback for Modal close
-});
+$(document).ready(function() {
+  $('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '4%', // Starting top style attribute
+    endingTop: '10%', // Ending top style attribute
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+    },
+    complete: function() {} // Callback for Modal close
+  });
 
-// $('#dropdown3').click(() => console.log($(this)))
-// $('.d3').click(() => {
-//   $('#foot').attr('id', $(this).html())
-// })
-$('#submit').click(() => {
-  let arr = $('.collapsible-body')
-  for(let i = 0; i < arr.length; i++) {
-    console.log(arr[i].attributes[2].value)
-  }
+  const arr = $('.collapsible-body')
+  $('#search').click(function() {
+    localStorage.setItem('stat1', arr[0].attributes[2].value)
+    localStorage.setItem('stat2', arr[1].attributes[2].value)
+    localStorage.setItem('constraint', arr[2].attributes[2].value)
+    window.location.href = "results.html"
+  })
 })
-
-
-// $('#submit').click(() => console.log('done'))
