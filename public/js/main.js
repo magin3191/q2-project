69ed9923effbb5954b4797530ddf8e53ad664294
Ross <ross.tienken99@gmail.com>
2017-12-11 14:39:18 -0700

    fixed tabs and login/signup pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(() => {
   $('.modal').modal({
     dismissible: true, // Modal can be dismissed by clicking outside of the modal
     opacity: .5, // Opacity of modal background
@@ -8,72 +8,14 @@ $(document).ready(function() {
     endingTop: '10%', // Ending top style attribute
     ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
     },
-    complete: function() {} // Callback for Modal close
+    complete: () => {} // Callback for Modal close
   });
 
   const arr = $('.collapsible-body')
-  $('#search').click(function() {
+  $('#search').click(() => {
     localStorage.setItem('stat1', arr[0].attributes[2].value)
     localStorage.setItem('stat2', arr[1].attributes[2].value)
     localStorage.setItem('constraint', arr[2].attributes[2].value)
     window.location.href = "results.html"
   })
-
-
-  $('#signUpMobile').submit((event) => {
-    event.preventDefault();
-
-    const firstName = $('#signFnMobile').val().trim();
-    const lastName = $('#signLnMobile').val().trim();
-    const email = $('#signEmailMobile').val().trim();
-    const password = $('#signPassMobile').val();
-
-    if (!firstName) {
-      return Materialize.toast('First name must not be blank', 3000);
-    }
-
-    if (!lastName) {
-      return Materialize.toast('Last name must not be blank', 3000);
-    }
-
-    if (!email) {
-      return Materialize.toast('Email must not be blank', 3000);
-    }
-
-    if (email.indexOf('@') < 0) {
-      return Materialize.toast('Email must be valid', 3000);
-    }
-
-    if (!password || password.length < 8) {
-      return Materialize.toast(
-        'Password must be at least 8 characters long',
-        3000
-      );
-    }
-    console.log(firstName)
-    console.log(lastName)
-    console.log(email)
-    console.log(password)
-
-    const options = {
-      contentType: 'application/json',
-      data: JSON.stringify({
-        firstName,
-        lastName,
-        email,
-        password
-      }),
-      dataType: 'json',
-      type: 'POST',
-      url: '/users'
-    };
-
-    $.ajax(options)
-      .done(() => {
-        window.location.href = '/index.html';
-      })
-      .fail(($xhr) => {
-        Materialize.toast($xhr.responseText, 3000);
-      });
-  });
 })
