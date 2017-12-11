eefbb38b1f4503f9261415789ee40583b94c5e3d
Matt <magin3191@gmail.com>
2017-12-11 10:08:01 -0700

    some changes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-var port = process.env.PORT || 3000
-var environment = process.env.NODE_ENV || 'development'
-var config = require('./knexfile')[environment]
-var knex = require('knex')(config)
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const knex = require('knex')(config)
 module.exports = knex
 
-
+app.use(bodyParser.json())
 
 app.use(express.static('public'))
 
@@ -15,41 +15,39 @@ app.listen(port, function() {
   console.log(`listening on port ${port}`)
 })
 
-app.get('/stats/players', function(req,res,next){
+app.get('/stats/players', function(req, res, next) {
+  knex
+    .select('stats_db')
+    .from('players')
+    .where('id', req.params.id)
+    .then(function(table) {
+      res.send(table)
+    })
+})
 
+app.get('/stats/teams', function(req, res, next) {
+  knex
+    // .select('stats_db')
+    // .from('teams')
+    // .where('id', req.params.id)
+    // .then(function(table) {
+    //   res.send(table)
+    })
+})
+
+app.get('/users/:id', function(req, res, next) {
+  // knex('users').where('id',req.params.id)
+  // .then(function(result){
+  //   res.send(result)
   })
-
 })
 
-app.get('/stats/teams',function(req,res,next){
+app.post('/users/', function(req, res, next) {})
 
-})
+app
+  .route('/favorites')
+  .get(function(req, res, next) {})
+  .post(function(req, res, next) {})
+  .delete(function(req, res, next) {})
 
-
-
-app.get('/users/:id',function(req,res,next){
-
-})
-
-
-app.post('/users/',function(req,res,next){
-
-})
-
-
-
-
-app.route('/favorites')
-  .get(function(req,res,next){
-
-  })
-  .post(function(req,res,next){
-
-  })
-  .delete(function(req,res,next){
-
-  })
-
-app.get('/token',function(req,res,next){
-
-})
+app.get('/token', function(req, res, next) {})
