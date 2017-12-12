424976175a7fd3435f88098dc0ec0f1af98982a4
Matt <magin3191@gmail.com>
2017-12-12 14:22:04 -0700

    routes

--- routes/stats.js
+++ routes/stats.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
 const cutDownData =require('../js/db.js').cutDownData
 
+app.use(bodyParser.json())
+
 router.post('/stats',function(req,res){
-  cutDownData(constraint, domStat1, domStat2).then(function(results){
+  cutDownData(constraint, domStat1, domStat2)
+  .then(function(results){
     res.send(results)
   })
 })
