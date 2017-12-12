92a7580aa11d99cbd89cb33fe1c4c8c1383b3bfc
caelinsheahan <caelinsheahan@gmail.com>
2017-12-12 15:00:52 -0700

    most of backend stats manip working

--- js/regres.js
+++ js/regres.js
@@ -1,12 +1,34 @@
 const regression = require('regression')
-const result = regression.linear([[24, 22], [44, 2], [33, 7]]) //([[stat1(player1), stat2(player1)]]); knex('players').where()
-const gradient = result.equation[0]
-const yIntercept = result.equation[1]
 
+function runRegression(arr, stat1, stat2) {
+  let playerNames = []
+  let playerStatPoints = []
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    playerNames.push(arr[i].Player)
+    playerStatPoints.push([
+      parseFloat(arr[i][stat1]),
+      parseFloat(arr[i][stat2])
+    ])
+  }
+  const regressLinear = regression.linear(playerStatPoints)
+
+  for (let i = 0; i < playerStatPoints.length; i++) {
+    result.push({
+      player: playerNames[i],
+      regresspoints: regressLinear.points[i],
+      correl: regressLinear.points[i][0] + regressLinear.points[i][1],
+      stat1: playerStatPoints[i][0],
+      stat2: playerStatPoints[i][1],
+      confidence: regressLinear.r2
+    })
+  }
+  return result
+}
+
+module.exports = { runRegression }
 // result looks like this: { points: [ [ 24, 19.81 ], [ 44, 0.21 ], [ 33, 10.99 ] ],
 // predict: [Function: predict],
 // equation: [ -0.98, 43.33 ],
 // r2: 0.89,
 // string: 'y = -0.98x + 43.33' }
-
-module.exports = {result}
