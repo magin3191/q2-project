4a9cfba97de87af5b5966fdf36d0f4a618d07981
Ross <ross.tienken99@gmail.com>
2017-12-12 22:09:06 -0700

    heroku

--- knexfile.js
+++ knexfile.js
@@ -6,37 +6,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/stats_db',
-
-    seeds: {
-      directory: './db/seeds'
-    },
-    migrations: {
-      directory: './db/migrations'
-    }
-
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    migrations: {
-      directory: path.join(__dirname, 'db', 'migrations')
-    }
   },
 
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
-    seeds: {
-      directory: path.join(__dirname, 'db', 'seeds')
-    },
-    migrations: {
-      directory: path.join(__dirname, 'db', 'migrations')
-    }
-  }
 
 };
