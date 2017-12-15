9c707e7cf857dcba8c08ae5ef6bf878bb7d57734
Ross <ross.tienken99@gmail.com>
2017-12-14 19:09:52 -0700

    favorites done

--- public/js/results.js
+++ public/js/results.js
@@ -3,7 +3,31 @@ $(document).ready(() => {
   const search_id = localStorage.getItem('search_id')
   const stat1 = localStorage.getItem('stat1')
   const stat2 = localStorage.getItem('stat2')
+  const ball = $('#ball')
   $('#confidence').html('Confidence = ' + (results[0].confidence * 100).toFixed(2) + '%')
+
+  let favArr
+  let oldFav
+  $.getJSON('/favorites')
+    .done((favorites) => {
+      favArr = favorites
+      oldFav = favArr[3]
+      for (let idx in favorites) {
+        if (favorites[idx].favorite === search_id) {
+          ball
+          .css({
+              'color': 'orange'
+            })
+            .attr({
+              'data-tooltip': 'Remove Favorite'
+            })
+        }
+      }
+
+    })
+    .fail(() => {})
+
+
   let tr = $('<tr/>')
   tr.append("<td>" + "<h5>Rank</h5>" + "</td>")
   tr.append("<td>" + "<h5>Player</h5>" + "</td>")
@@ -12,85 +36,124 @@ $(document).ready(() => {
   tr.append("<td>" + `<h5>${stat2}</h5>` + "</td>")
   $('table').append(tr)
   for (var i = 0; i < results.length; i++) {
-      let tr = $('<tr/>')
-      tr.append("<td>" + '#' + (i+1) + "</td>")
-      tr.append("<td>" + results[i].player + "</td>")
-      tr.append("<td>" + results[i].correl.toFixed(2) + "</td>")
-      tr.append("<td>" + results[i].stat1 + "</td>")
-      tr.append("<td>" + results[i].stat2 + "</td>")
-      $('table').append(tr)
-    }
-  const ball = $('#ball')
-  if (ball.css('color') === 'rgba(0, 0, 0, 0)') {
+    let tr = $('<tr/>')
+    tr.append("<td>" + '#' + (i + 1) + "</td>")
+    tr.append("<td>" + results[i].player + "</td>")
+    tr.append("<td>" + results[i].correl.toFixed(2) + "</td>")
+    tr.append("<td>" + results[i].stat1 + "</td>")
+    tr.append("<td>" + results[i].stat2 + "</td>")
+    $('table').append(tr)
   }
-  else {
 
-  }
   ball.click(function() {
     $.getJSON('/token')
       .done((loggedIn) => {
         if (loggedIn) {
           if ($(this).css('color') === 'rgba(0, 0, 0, 0)') {
-            addFavorite()
+            if (favArr.length >= 4) {
+              updateFavorite(favArr[3].favorite)
+              addFavorite()
+            } else if (favArr.length < 4) {
+              addFavorite()
+            }
           } else {
-            removeFavorite()
+            removeFavorite(search_id)
           }
         } else {
           Materialize.toast('You must be logged in to add favorites', 2500)
         }
       })
   })
+
+
   const addFavorite = () => {
-    event.preventDefault();
+    event.preventDefault()
 
+    const options = {
+      contentType: 'application/json',
+      data: search_id,
+      dataType: 'json',
+      type: 'POST',
+      url: '/favorites'
+    }
 
-      const options = {
-        contentType: 'application/json',
-        data: search_id,
-        dataType: 'json',
-        type: 'POST',
-        url: '/favorites'
-      };
-
-      $.ajax(options)
-        .done(() => {
-          ball
-            .css({'color': 'orange', 'animation': 'fade1 .5s linear 1'})
-            .attr({'data-tooltip': 'Remove Favorite'})
-            .tooltip();
-          Materialize.toast('Favorite added', 1500)
-        })
-        .fail(() => {
-          Materialize.toast('Unable to add favorite', 2000);
-        });
+    $.ajax(options)
+      .done(() => {
+        ball
+          .css({
+            'color': 'orange',
+            'animation': 'fade1 .5s linear 1'
+          })
+          .attr({
+            'data-tooltip': 'Remove Favorite'
+          })
+          .tooltip()
+        Materialize.toast('Favorite added', 1500)
+        $.getJSON('/favorites')
+          .done((favorites) => {
+            favArr = favorites
+          })
+          .fail(() => {})
+      })
+      .fail(() => {
+        Materialize.toast('Unable to add favorite', 2000)
+      })
   }
   const removeFavorite = () => {
-    event.preventDefault();
+    event.preventDefault()
 
-      const options = {
-        contentType: 'application/json',
-        data: search_id,
-        dataType: 'json',
-        type: 'DELETE',
-        url: '/favorites'
-      };
+    const options = {
+      contentType: 'application/json',
+      data: search_id,
+      dataType: 'json',
+      type: 'DELETE',
+      url: '/favorites'
+    }
 
-      $.ajax(options)
-        .done(() => {
-          ball
-            .css({'color': 'rgba(0, 0, 0, 0)', 'animation': 'fade2 .5s linear 1'})
-            .attr({'data-tooltip': 'Add Favorite'})
-            .tooltip()
-          Materialize.toast('Favorite removed', 1500)
-        })
-        .fail(() => {
-          Materialize.toast(
-            'Unable to remove favorite',
-            3000
-          );
-        });
+    $.ajax(options)
+      .done(() => {
+        ball
+          .css({
+            'color': 'rgba(0, 0, 0, 0)',
+            'animation': 'fade2 .5s linear 1'
+          })
+          .attr({
+            'data-tooltip': 'Add Favorite'
+          })
+          .tooltip()
+        Materialize.toast('Favorite removed', 1500)
+      })
+      .fail(() => {
+        Materialize.toast(
+          'Unable to remove favorite',
+          3000
+        )
+      })
 
   }
+
+  const updateFavorite = (sId) => {
+    event.preventDefault()
+
+    const options = {
+      contentType: 'application/json',
+      data: sId,
+      dataType: 'json',
+      type: 'DELETE',
+      url: '/favorites'
+    }
+
+    $.ajax(options)
+      .done(() => {
+        $.getJSON('/favorites')
+          .done((favorites) => {
+            favArr = favorites
+          })
+          .fail(() => {
+            Materialize.toast('Unable to update favorites', 3000)
+          })
+        })
+    }
   $('ul.tabs').tabs({
     swipeable: true,
     responsiveThreshold: 1920
--- public/js/scatter.js
+++ public/js/scatter.js
@@ -1,60 +1,61 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-const results = JSON.parse(localStorage.getItem('results'))
-const stat1 = localStorage.getItem('stat1')
-const stat2 = localStorage.getItem('stat2')
+  const results = JSON.parse(localStorage.getItem('results'))
+  const stat1 = localStorage.getItem('stat1')
+  const stat2 = localStorage.getItem('stat2')
 
-var chart = c3.generate({
-bindto: '#chart',
+  var chart = c3.generate({
+    bindto: '#chart',
+    point: {
+      r: 10
+    },
     data: {
-        xs: {
-            'stat1': 'stat2',
-        },
-        columns: [
-          [
-            'stat1',
-            results[0].regresspoints[0],
-            results[1].regresspoints[0],
-            results[2].regresspoints[0],
-            results[3].regresspoints[0],
-            results[4].regresspoints[0],
-            results[5].regresspoints[0],
-            results[6].regresspoints[0],
-            results[7].regresspoints[0],
-            results[8].regresspoints[0],
-            results[9].regresspoints[0]
-          ],
-          [
-            'stat2',
-            results[0].regresspoints[1],
-            results[1].regresspoints[1],
-            results[2].regresspoints[1],
-            results[3].regresspoints[1],
-            results[4].regresspoints[1],
-            results[5].regresspoints[1],
-            results[6].regresspoints[1],
-            results[7].regresspoints[1],
-            results[8].regresspoints[1],
-            results[9].regresspoints[1]
-          ]
-        ],
-        type: 'scatter'
+      xs: {
+        'stat1': 'stat2',
       },
+      columns: [
+        [
+          'stat1',
+          results[0].regresspoints[0],
+          results[1].regresspoints[0],
+          results[2].regresspoints[0],
+          results[3].regresspoints[0],
+          results[4].regresspoints[0],
+          results[5].regresspoints[0],
+          results[6].regresspoints[0],
+          results[7].regresspoints[0],
+          results[8].regresspoints[0],
+          results[9].regresspoints[0]
+        ],
+        [
+          'stat2',
+          results[0].regresspoints[1],
+          results[1].regresspoints[1],
+          results[2].regresspoints[1],
+          results[3].regresspoints[1],
+          results[4].regresspoints[1],
+          results[5].regresspoints[1],
+          results[6].regresspoints[1],
+          results[7].regresspoints[1],
+          results[8].regresspoints[1],
+          results[9].regresspoints[1]
+        ]
+      ],
+      type: 'scatter'
+    },
     axis: {
-        x: {
-           min: 0,
-            label: `${stat2}`,
-            tick: {
-                fit: false
-            }
-        },
-        y: {
-         min: 0,
-            label: `${stat1}`,
-            tick: {
-                fit: false
-            }
+      x: {
+        label: `${stat2}`,
+        tick: {
+          fit: false
         }
+      },
+      y: {
+        label: `${stat1}`,
+        tick: {
+          fit: false
+        }
+      }
     }
-})
+  })
 })
